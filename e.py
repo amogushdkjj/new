18c55f529aca9b50d6f420a794fa5dab8511317f
amogushdkjj g <ceceb0417@gmail.com>
2023-09-06 05:38:19 +0000

    commit

---
+++ e.py
@@ -0,0 +1,4 @@
+distance = int(input("enter the distance in light years: "))
+speed = int(input("enter the speed in fraction of speed of light: "))
+time = distance/speed
+print("it will take ", time, " years")
